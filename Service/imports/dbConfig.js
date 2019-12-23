3a57073001d1dc2ded172b1aae90a9af9859835c
Oozdil <37013255+Oozdil@users.noreply.github.com>
2019-12-23 18:07:35 +0300

    SOA Teslim
    SOA Teslim

--- Service/imports/dbConfig.js
+++
@@ -1,8 +0,0 @@
-module.exports =  dbConfig = {    
-    driver: 'msnodesqlv8',
-    connectionString:
-	'Driver={SQL Server Native Client 11.0};'+
-	'Server={localhost\\SQLExpress};'+
-	'Database={LocallyDB};'+
-	'Trusted_Connection={yes};'
-};
